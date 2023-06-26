265ecdbcdbe44b7811196a1dff33ce94c84bc6ff
kosolapov_d <kosolapov.d@orionnet.ru>
2023-06-26 15:04:48 +0700

    add config file

---
+++ app/config/config.js
@@ -0,0 +1,20 @@
+function getEnv(name, defaults) {
+    if (!process.env[name]) {
+        return defaults
+    } else {
+        return process.env[name];
+    }
+}
+
+const config = {
+    db: {
+        host: 'localhost',
+        port: 5432,
+        name: 'xxx',
+
+        user: 'xxx',
+        password: 'xxx'
+    }
+};
+
+module.exports = config;
